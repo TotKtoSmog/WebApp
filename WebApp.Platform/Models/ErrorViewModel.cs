e716b2ea3778b704016bc5ddbcc3f95e785577f3
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-03-20 22:03:26 +0300

    Добавьте файлы проекта.

---
+++ WebApp.Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp.Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
