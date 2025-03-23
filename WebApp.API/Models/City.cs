bed1152d71968c1484360b29ff8d4799157c1d18
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-03-23 22:59:22 +0300

    Настройка подключения к базе данных

---
+++ WebApp.API/Models/City.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.API.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public required string Title {  get; set; }
+        public string? Description { get; set; }
+        public string? MainPictureLink { get; set; }
+        public string? PictureAtHomeLink { get; set; }
+        public required string PageName { get; set; }
+        public bool PageVisible { get; set; }
+
+    }
+}
--- WebApp.API/Program.cs
+++ WebApp.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.API.Context;
 
 namespace WebApp.API
 {
@@ -7,16 +9,16 @@ namespace WebApp.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddDbContextFactory<CityContext>(o => o.UseNpgsql(connectionString));
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
