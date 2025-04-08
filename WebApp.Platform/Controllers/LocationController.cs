944b4ad0a7b382029a2c44360e0f3fc9fd12d03f
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-04-08 22:47:32 +0300

    Рефакторинг LocationController

--- WebApp.Platform/Controllers/LocationController.cs
+++ WebApp.Platform/Controllers/LocationController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using System.Security.Claims;
 using WebApp.API.Models;
 using WebApp.Platform.Models;
 using WebApp.Platform.Services.Interfaces;
@@ -11,16 +10,11 @@ namespace WebApp.Platform.Controllers
     {
         private readonly ILogger<LocationController> _logger;
         private readonly ILocationService _locationService;
-        private readonly IClientIpService _clientIpService;
-        private readonly IJwtTokenService _jwtTokenService;
 
-        public LocationController(ILogger<LocationController> logger, ILocationService cityService, 
-            IClientIpService clientIpService, IJwtTokenService jwtTokenService)
+        public LocationController(ILogger<LocationController> logger, ILocationService cityService)
         {
             _logger = logger;
             _locationService = cityService;
-            _clientIpService = clientIpService;
-            _jwtTokenService = jwtTokenService;
         }
         [HttpGet]
         public async Task<IActionResult> Index(string pageName)
@@ -32,12 +26,9 @@ namespace WebApp.Platform.Controllers
             if (User.Identity.IsAuthenticated)
             {
                 var token = Request.Cookies["jwt_token"];
-                if (!string.IsNullOrEmpty(token))
-                {
-                    var name = _jwtTokenService.GetNameUserFromToken(token);
-                    if (!string.IsNullOrEmpty(name))
-                        ViewData["FIO"] = name;
-                }
+                var name = _locationService.GetUserFirstAndLastName(token ?? "");
+                if (!string.IsNullOrEmpty(name))
+                    ViewData["FIO"] = name;
             }
             return View(locationInformation);
         }
@@ -45,20 +36,8 @@ namespace WebApp.Platform.Controllers
         [HttpPost]
         public async Task<IActionResult> AddReview(string pageName, Feedback model)
         {
-            model.SenderIpAddress = _clientIpService.GetClientIp();
-            model.DateTime = DateTime.UtcNow;
-            if (User.Identity.IsAuthenticated)
-            {
-                var token = Request.Cookies["jwt_token"];
-                if (!string.IsNullOrEmpty(token))
-                {
-                    var id = _jwtTokenService.GetUserIdFromToken(token);
-                    if (!string.IsNullOrEmpty(id) && int.TryParse(id, out int userId))
-                        model.IdUser = userId;
-                }
-            }
-
-            await _locationService.CreateFeedbackAsync(model);
+            var token = Request.Cookies["jwt_token"];
+            await _locationService.CreateFeedbackAsync(model, token ?? "");
             return RedirectToAction("Index", new { pageName });
         }
 
--- WebApp.Platform/Services/Interfaces/ILocationService.cs
+++ WebApp.Platform/Services/Interfaces/ILocationService.cs
@@ -10,5 +10,7 @@ namespace WebApp.Platform.Services.Interfaces
         public Task<List<FeedbackView>> GetFeedbackViewByIdLocationAsync(int idLocation);
         public Task<AllLocationInformation> GetAllLocationInformationAsync(string pageName);
         public Task<Feedback> CreateFeedbackAsync(Feedback feedback);
+        public Task<Feedback> CreateFeedbackAsync(Feedback feedback, string token);
+        public string? GetUserFirstAndLastName(string token);
     }
 }
