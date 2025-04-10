8952bd219693370bc47776c384a0a52dda1f9d1a
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-04-10 22:47:00 +0300

    Добавление AdminPanel

---
+++ WebApp.Platform/Controllers/AdminPanelController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Platform.Controllers
+{
+    [Authorize(Roles="Admin")]
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
