3e457c8bc8778f0e08f1498ee9484b024e684895
Alexey Ustinov <TotKtoSmog513@gmail.com>
2025-04-30 15:57:22 +0300

    Создание интерфейса для администрирования отзывов

---
+++ WebApp.Platform/Areas/Admin/Controllers/FeedbackController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.API.Models;
+
+namespace WebApp.Platform.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class FeedbackController : Controller
+    {
+        public async Task<IActionResult> Index()
+            => View(new List<Feedback>());
+    }
+}
